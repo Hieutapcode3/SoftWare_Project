6c8235d69ee7120fccb6997326bf40cfaf33853b
Hieutapcode3 <hieupham9155@gmail.com>
2024-09-16 22:22:27 +0700

    add form page & navbar in index.html

--- asset/js/index.js
+++ asset/js/index.js
@@ -32,3 +32,32 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+const navBarButton = document.querySelector(".nav-bar");
+const containerNavBar = document.querySelector(".container-nav-bar");
+const closeButton = document.querySelector(".close-btn button");
+const ulElement = document.querySelector(".container-nav-bar ul");
+navBarButton.addEventListener("click", () => {
+  containerNavBar.style.display = "block";
+  setTimeout(() => {
+    containerNavBar.classList.add("show");
+  }, 100);
+});
+closeButton.addEventListener("click", () => {
+  containerNavBar.classList.remove("show");
+  setTimeout(() => {
+    containerNavBar.style.display = "none";
+  }, 500);
+});
+document.addEventListener("click", (event) => {
+  const isClickInside =
+    ulElement.contains(event.target) ||
+    closeButton.contains(event.target) ||
+    navBarButton.contains(event.target);
+
+  if (!isClickInside) {
+    containerNavBar.classList.remove("show");
+    setTimeout(() => {
+      containerNavBar.style.display = "none";
+    }, 500);
+  }
+});
